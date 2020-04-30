95762e474af02206647d2d2f50f3f806a3332cae
Anthony M Hernandez <55599397+asvka@users.noreply.github.com>
2020-04-30 14:16:17 -0700

    Designed recipe_book database, with 4 tables created in migrations

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const helmet = require('helmet')
+
+const server = express()
+const port = process.env.PORT || 7777
+
+server.use(helmet())
+server.use(express.json())
+
+server.use((err, req, res, next) => {
+	console.log(err)
+	res.status(500).json({
+		message: "An error was detected.",
+	})
+})
+
+server.listen(port, () => {
+	console.log(`Running at http://localhost:${port}`)
+})
